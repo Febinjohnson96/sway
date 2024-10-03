169e3e2c2f89e50cb61eeed02aa64c18aca5ba7b
Febin Johnson <febinnj1996@gmail.com>
2024-10-03 22:00:57 +0530

    design completed for auth

--- lib/presentation/auth/ui/auth_screen.dart
+++ lib/presentation/auth/ui/auth_screen.dart
@@ -4,6 +4,7 @@ import 'package:sway/config/app_colors.dart';
 import 'package:sway/config/app_typography.dart';
 import 'package:sway/presentation/widgets/authentication_button.dart';
 import 'package:sway/presentation/widgets/sw_scaffold_with_padding.dart';
+import 'package:sway/util/app_logger.dart';
 
 class AuthScreen extends StatelessWidget {
   const AuthScreen({super.key});
@@ -12,7 +13,8 @@ class AuthScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SwScaffoldWithPadding(
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text(
             "Get Started",
@@ -23,9 +25,22 @@ class AuthScreen extends StatelessWidget {
             style: AppTypography.sWayMedium16
                 .copyWith(color: AppColors.primary500),
           ),
-          SizedBox(height: 24.h),
-          const Align(
-              alignment: Alignment.center, child: AuthenticationButton())
+          SizedBox(height: 20.h),
+          Align(
+              alignment: Alignment.center,
+              child: AuthenticationButton(
+                onTap: () => AppLogger.infolog("continue with google"),
+              )),
+          SizedBox(
+            height: 12.h,
+          ),
+          Center(
+            child: Text(
+              "By signing up you agree to our Terms and Privacy Policy",
+              style: AppTypography.sWayMedium12
+                  .copyWith(color: AppColors.primary900),
+            ),
+          )
         ],
       ),
     );
--- lib/presentation/widgets/authentication_button.dart
+++ lib/presentation/widgets/authentication_button.dart
@@ -1,28 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:sway/config/app_colors.dart';
 import 'package:sway/config/app_typography.dart';
+import 'package:sway/gen/assets.gen.dart';
 
 class AuthenticationButton extends StatelessWidget {
-  const AuthenticationButton({super.key});
+  const AuthenticationButton({super.key, this.onTap});
+  final VoidCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 341.w,
-      height: 56.h,
-      decoration: BoxDecoration(
-        border: Border.all(color: AppColors.primary200,width: 1),
-          borderRadius: BorderRadius.circular(10), color: AppColors.primary0),
-      child: Row(
-        children: [
-          Text(
-            "continue with google",
-            style: AppTypography.sWayMedium16.copyWith(
-              color: AppColors.primary900,
+    return GestureDetector(
+      onTap: () => onTap?.call(),
+      child: Container(
+        width: 341.w,
+        height: 56.h,
+        decoration: BoxDecoration(
+            border: Border.all(color: AppColors.primary200, width: 1),
+            borderRadius: BorderRadius.circular(10),
+            color: AppColors.primary0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(Assets.vector.common.googleicon),
+            SizedBox(
+              width: 20.w,
             ),
-          )
-        ],
+            Text(
+              "continue with google",
+              style: AppTypography.sWayMedium16.copyWith(
+                color: AppColors.primary900,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
