e3b1b7e479406b6fc859fa1dc143c358a5e3a2fc
Febin Johnson <febinnj1996@gmail.com>
2024-10-04 19:30:12 +0530

    added firebase fully

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,14 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:sway/config/app_routes.dart';
+import 'package:sway/firebase_options.dart';
 import 'package:sway/injector.dart' as injector;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   await injector.init();
   runApp(const MyApp());
 }
--- lib/managers/auth/authentication.dart
+++ lib/managers/auth/authentication.dart
@@ -1,1 +1,16 @@
-class Authentication {}
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class Authentication {
+  Future<UserCredential> googleSignin() async {
+    final GoogleSignInAccount? user = await GoogleSignIn().signIn();
+    final GoogleSignInAuthentication? auth = await user?.authentication;
+
+    final credential = GoogleAuthProvider.credential(
+      accessToken: auth?.accessToken,
+      idToken: auth?.idToken,
+    );
+
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+}
